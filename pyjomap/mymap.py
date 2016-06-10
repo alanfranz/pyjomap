249033b20386a509c84db4f250cc3363eaca7184
Alan Franzoni <username@franzoni.eu>
2016-06-10 20:24:53 +0200

    Enh: better test organization

--- pyjomap/mymap.py
+++ pyjomap/mymap.py
@@ -2,13 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from inspect import getargspec, getmembers
-from unittest import TestCase, main
 from copy import deepcopy
-import logging
 import codecs
-
 from collections import Mapping as CollectionMapping
-from genty import genty, genty_dataset
 
 
 # mapper will be invoked with the a value of type "origin" and the reference object,
@@ -223,84 +219,11 @@ def getfieldsandvalues(o):
     return set([(name, value) for name, value in getmembers(o) if not name.startswith("_") and not callable(value)])
 
 
-class MyItem(object):
-    def __init__(self, a, b, c=None, d=None, e=None):
-        self.a = a
-        self._b = b
-        self._private = 1
-        self.c = c
-        self.d = d
-        self.e = e
-
-    @property
-    def b(self):
-        return self._b
-
-    @b.setter
-    def b(self, value):
-        self._b = value
-
-    def __eq__(self, other):
-        return getattr(other, "__dict__", None) is not None and self.__dict__ == other.__dict__
-
-    def __str__(self):
-        return "a:{0}   b:{1}   c:{2}  d:{3}".format(self.a, self._b, self.c, self.d)
-
-    def __repr__(self):
-        return str(self)
 
 
-class Other(object):
-    def __init__(self, r, s):
-        self.r = r
-        self.s = s
-
-    def __eq__(self, other):
-        return getattr(other, "__dict__", None) is not None and self.__dict__ == other.__dict__
 
 
-class TestFieldInspection(TestCase):
-    def test_fields(self):
-        i = MyItem(1, "asd")
-        self.assertEquals(set(["a", "b", "c", "d", "e"]), getfields(i))
-
-    def test_bind(self):
-        item = bind({"a": 7, "b": "asd", "c": [3, 4], "d": {}, "e": []}, MyItem)
-        expected = MyItem(7, "asd", [3, 4], {}, [])
-        self.assertEqual(expected, item)
-
-    def test_bind_fails_if_not_enough_data(self):
-        self.assertRaises(BindException, bind, {"a": 7, "b": "asd"}, MyItem)
 
 
-class MyIntSubclass(int):
-    pass
 
 
-@genty
-class TestMappingFromDict(TestCase):
-    DICT_IN = {
-        "a": 5,
-        "b": "whatààà",
-        "c": [{1: 2}, {1: 2}],
-        "d": {"1": 1, "2": 2},
-        "e": {"r": 5, "s": 6}
-    }
-
-    @genty_dataset(
-        (DICT_IN, MyItem(MyIntSubclass(7), "asd", ({2: 3}, {4: 5}), {10: "w", 20: "xxx"}, e=Other(9, 10)),
-         MyItem(MyIntSubclass(5), "whatààà", ({1: 2}, {1: 2}), {1: "1", 2: "2"}, e=Other(5, 6))),
-        (DICT_IN, MyItem("7", u"asd", [{"2": "3"}], {10: "w", 20: "xxx"}, e=Other("a", "b")),
-         MyItem("5", u"whatààà", [{"1": "2"}, {"1": "2"}], {1: "1", 2: "2"}, e=Other("5", "6"))),
-    )
-    def asd(self, source_value, reference, expected):
-        registry = DefaultMapperRegistry(conversion_encoding="utf-8")
-        instance = registry.mapobj(source_value, reference)
-        self.assertEquals(expected, instance)
-
-
-if __name__ == "__main__":
-    import sys
-
-    logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
-    main()
